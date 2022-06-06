55d823b08c536c1761d0d37cfd53cb537f7ea875
mikkie <mikkie423@gmail.com>
2022-06-07 11:32:33 +1200

    ready to start

---
+++ main/CS103.2_Group-Project/CS103.2_Group-Project/CS103.2_Group-Project.cpp
@@ -0,0 +1,9 @@
+// CS103.2_Group-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Mikkie, Sese, Delear - O for Owesome
+
+#include <iostream>
+
+int main()
+{
+    
+}
